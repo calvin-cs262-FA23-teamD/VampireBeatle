8e32dcf60233792e2e5cfca6f0771a5bdffc4a48
aam33 <113713028+aam33@users.noreply.github.com>
2025-12-05 15:00:56 -0500

    min functional photo picking app

--- VampireBeatle/app/tabs/index.tsx
+++ VampireBeatle/app/tabs/index.tsx
@@ -5,27 +5,47 @@ import { Image } from "expo-image";
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 
+// Use an image picker step 2 120525 AM
+import * as ImagePicker from 'expo-image-picker';
+// Use the selected image step 4 120525 AM
+import { useState } from "react";     // declare a state variable using the useState hook
+
 // ~ does not resolve to root 120525 AM
 // NOTE: @ symbol is a custom path alias for importing custom components and other modules instead of relative paths, configured in tsconfig.json
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);  // step 4 use an image picker 120525 AM
+
+  // Use an image picker step 2 120525 AM
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log(result);
+    } else {    // result canceled
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
       <View>
         <Text style={styles.text}>Home screen</Text>
         <Link href="/tabs/about" style={styles.button}>Go to About screen</Link>
       </View>
-      {/*}
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/tabs/about" style={styles.button}>Go to About screen</Link>*/}
     </View>
   );
 }
--- VampireBeatle/components/Button.tsx
+++ VampireBeatle/components/Button.tsx
@@ -6,24 +6,25 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 type Props = {
     label: string;
     theme?: 'primary';  // nullable
+    onPress?: () => void;   // step 3 use an image picker. call pickImageAsync() on the Button component
 };
 
 // created after StyleSheet (below) 120525 AM
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
     if (theme === 'primary') {
-        return (
-            <View style={[styles.buttonContainer,
-                { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }
-            ]}>
-                <Pressable style={[styles.button, { backgroundColor: '#fff' }]}
-                    onPress={() =>
-                        alert('You pressed the button.')}>
-                    <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
-                    <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
-                </Pressable>
-            </View>
-        );
-    }
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+        ]}>
+        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
+          <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+        </Pressable>
+      </View>
+    );
+  }
 
     return (
         <View style={styles.buttonContainer}>
