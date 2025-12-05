b86ed292bd5b66caeb2434bc2833181fd7fd35af
aam33 <113713028+aam33@users.noreply.github.com>
2025-12-05 11:36:42 -0500

    delete app and create a new TS app yayayy

--- styles/colors.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const COLORS = {
-  background: '#0a0e0f',
-  offWhite: '#f0f5f5',
-  orange: '#ff6900',
-  buttonBackground: '#1f2e2e',
-  darkOrange: '#a23600',
-};
